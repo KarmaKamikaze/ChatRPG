a9cf9d299cc6152eb6467e55bb43e6a45d037ebe
damniko <ndam20@student.aau.dk>
2023-10-12 12:47:15 +0200

    Set up Blazor Server project and solution

---
+++ ChatRPG/ChatRPG/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatRPG.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
