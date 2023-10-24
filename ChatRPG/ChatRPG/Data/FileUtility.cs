c293c05a82572e4870a576bb80e9affb365c9dd3
KarmaKamikaze <nicolaijgn@gmail.com>
2023-10-24 12:25:07 +0200

    FileUnility class for handling saving of conversations in prototype

---
+++ ChatRPG/ChatRPG/Data/FileUtility.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace ChatRPG.Data;
+
+public class FileUtility
+{
+    public FileUtility(string saveDir = "Saves/")
+    {
+        SaveDir = saveDir;
+    }
+
+    public string SaveDir { get; set; }
+
+    public async Task UpdateSaveFileAsync(List<string> conversation)
+    {
+        string path = SetPath(DateTime.Now);
+        await using FileStream fs =
+            new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.None, bufferSize: 4096,
+                useAsync: true);
+        foreach (string message in conversation)
+        {
+            byte[] encodedMessage = Encoding.Unicode.GetBytes(message);
+            await fs.WriteAsync(encodedMessage, 0, encodedMessage.Length);
+        }
+    }
+
+    private string SetPath(DateTime timestamp)
+    {
+        // Add save directory and file name to path
+        string savePath = Path.Join(AppDomain.CurrentDomain.BaseDirectory, SaveDir);
+        string fileName = $"conversation {timestamp:dd-mm-yyyy HH-mm-ss}.txt";
+        return Path.Join(savePath, fileName);
+    }
+}
