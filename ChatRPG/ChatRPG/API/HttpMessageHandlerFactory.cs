a64961afb14733cba8abee648dfbca7576a157b4
KarmaKamikaze <nicolaijgn@gmail.com>
2023-10-28 00:41:35 +0200

    Add missing spaces in whitespace in HttpMessageHandlerFactory

--- ChatRPG/ChatRPG/API/HttpMessageHandlerFactory.cs
+++ ChatRPG/ChatRPG/API/HttpMessageHandlerFactory.cs
@@ -5,24 +5,25 @@ namespace ChatRPG.API;
 public class HttpMessageHandlerFactory : IHttpMessageHandlerFactory
 {
     private bool _useMocks;
+
     public HttpMessageHandlerFactory(IConfiguration configuration)
     {
         _useMocks = configuration.GetValue<bool>("UseMocks");
     }
 
-   public HttpMessageHandler CreateHandler(string name)
-   {
-       if (!_useMocks)
-       {
-           return new HttpClientHandler();
-       }
+    public HttpMessageHandler CreateHandler(string name)
+    {
+        if (!_useMocks)
+        {
+            return new HttpClientHandler();
+        }
 
-       var mockHttpMessageHandler = new MockHttpMessageHandler();
-       mockHttpMessageHandler.When("*")
-           .Respond(GenerateMockResponse);
+        var mockHttpMessageHandler = new MockHttpMessageHandler();
+        mockHttpMessageHandler.When("*")
+            .Respond(GenerateMockResponse);
 
-       return mockHttpMessageHandler;
-   }
+        return mockHttpMessageHandler;
+    }
 
     private static HttpResponseMessage GenerateMockResponse(HttpRequestMessage request)
     {
@@ -32,7 +33,7 @@ public class HttpMessageHandlerFactory : IHttpMessageHandlerFactory
                                                   {
                                                       "id": "chatcmpl-000",
                                                       "object": "chat.completion",
-                                                      "created": {{(int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds)}},
+                                                      "created": {{(int) (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds)}},
                                                       "model": "gpt-3.5-turbo",
                                                       "choices": [{
                                                           "index": 0,
