e6c616912062eaadad2bdc0afd106feb6d511637
mirakst <mstids20@student.aau.dk>
2023-11-09 11:25:50 +0100

    Restructured OpenAiLlmClient.cs and Campaign.razor

--- ChatRPG/API/IOpenAiLlmClient.cs
+++ ChatRPG/API/IOpenAiLlmClient.cs
@@ -2,20 +2,8 @@ namespace ChatRPG.API;
 
 public interface IOpenAiLlmClient
 {
-    Task<string> GetChatCompletion(OpenAiGptMessage input);
-    Task<string> GetChatCompletion(List<OpenAiGptMessage> inputs);
-    IAsyncEnumerable<string> GetStreamedChatCompletion(OpenAiGptMessage input);
-    IAsyncEnumerable<string> GetStreamedChatCompletion(List<OpenAiGptMessage> inputs);
+    Task<string> GetChatCompletion(params OpenAiGptMessage[] inputs);
+    IAsyncEnumerable<string> GetStreamedChatCompletion(params OpenAiGptMessage[] inputs);
 }
 
-public record ChatCompletionObject(string Id, string Object, int Created, string Model, Choice[] Choices, Usage Usage);
-
-public record Choice(int Index, Message Message, string FinishReason);
-
-public record Message(string Role, string Content);
-
-public record Usage(int PromptTokens, int CompletionTokens, int TotalTokens);
-
 public record OpenAiGptMessage(string Role, string Content);
-
-public record OpenAiGptInput(string Model, List<OpenAiGptMessage> Messages, double Temperature);
--- ChatRPG/API/OpenAiLlmClient.cs
+++ ChatRPG/API/OpenAiLlmClient.cs
@@ -9,50 +9,36 @@ public class OpenAiLlmClient : IOpenAiLlmClient
     private const string Model = "gpt-3.5-turbo";
     private const double Temperature = 0.7;
 
-    private readonly IHttpClientFactory _httpClientFactory;
     private readonly OpenAIAPI _openAiApi;
 
     public OpenAiLlmClient(IConfiguration configuration, IHttpClientFactory httpClientFactory)
     {
-        _httpClientFactory = httpClientFactory;
         _openAiApi = new OpenAIAPI(configuration.GetSection("ApiKeys").GetValue<string>("OpenAI") ?? string.Empty);
         _openAiApi.Chat.DefaultChatRequestArgs.Model = Model;
         _openAiApi.Chat.DefaultChatRequestArgs.Temperature = Temperature;
+        _openAiApi.HttpClientFactory = httpClientFactory;
     }
 
-    public async Task<string> GetChatCompletion(OpenAiGptMessage input)
-    {
-        List<OpenAiGptMessage> inputList = new List<OpenAiGptMessage> { input };
-        return await GetChatCompletion(inputList);
-    }
-
-    public async Task<string> GetChatCompletion(List<OpenAiGptMessage> inputs)
+    public async Task<string> GetChatCompletion(params OpenAiGptMessage[] inputs)
     {
         Conversation chat = CreateConversation(inputs);
 
         return await chat.GetResponseFromChatbotAsync();
     }
 
-    public IAsyncEnumerable<string> GetStreamedChatCompletion(OpenAiGptMessage input)
-    {
-        List<OpenAiGptMessage> inputList = new List<OpenAiGptMessage> { input };
-        return GetStreamedChatCompletion(inputList);
-    }
-
-    public IAsyncEnumerable<string> GetStreamedChatCompletion(List<OpenAiGptMessage> inputs)
+    public IAsyncEnumerable<string> GetStreamedChatCompletion(params OpenAiGptMessage[] inputs)
     {
         Conversation chat = CreateConversation(inputs);
 
         return chat.StreamResponseEnumerableFromChatbotAsync();
     }
 
-    private Conversation CreateConversation(List<OpenAiGptMessage> inputs)
+    private Conversation CreateConversation(params OpenAiGptMessage[] messages)
     {
-        if (inputs.IsNullOrEmpty()) throw new ArgumentNullException(nameof(inputs));
+        if (messages.IsNullOrEmpty()) throw new ArgumentNullException(nameof(messages));
 
         Conversation chat = _openAiApi.Chat.CreateConversation();
-        _openAiApi.HttpClientFactory = _httpClientFactory;
-        foreach (OpenAiGptMessage openAiGptInputMessage in inputs)
+        foreach (OpenAiGptMessage openAiGptInputMessage in messages)
         {
             chat.AppendMessage(ChatMessageRole.FromString(openAiGptInputMessage.Role), openAiGptInputMessage.Content);
         }
