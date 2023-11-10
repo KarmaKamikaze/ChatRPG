254100ca6cbf3ae6ba607d7a064a3b426814c2bc
Sarmisuper <72615675+Sarmisuper@users.noreply.github.com>
2023-11-10 15:01:35 +0100

    Update ResetPassword.cshtml.cs

--- ChatRPG/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ ChatRPG/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -50,7 +50,7 @@ namespace ChatRPG.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
