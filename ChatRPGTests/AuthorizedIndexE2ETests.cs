edbba2fbf720febd762993a1c0078a16f51cef5c
KarmaKamikaze <nicolaijgn@gmail.com>
2023-11-17 12:46:19 +0100

    Fix memberdata in tests

--- ChatRPGTests/AuthorizedIndexE2ETests.cs
+++ ChatRPGTests/AuthorizedIndexE2ETests.cs
@@ -332,33 +332,36 @@ public class AuthorizedIndexE2ETests : IDisposable
         new object[]
         {
             "The peaceful kingdom of Aldorath is now facing imminent dangers from a prophecy that " +
-            "foretells its downfall. The only hope lies in the legend of seven ancient artifacts rumored " +
-            "to have immense power. The players start as inexperienced adventurers summoned by the king."
+            "foretells its downfall. The only hope lies in the legend of seven ancient artifacts " +
+            "rumored to have immense power. The players start as inexperienced adventurers " +
+            "summoned by the king."
         },
         new object[]
         {
-            "In the remote, usually tranquil village of Silverpine, people have been mysteriously " +
-            "vanishing during the night. The players start as a group of investigators hired by the " +
-            "desperate villagers to discover the source of these unsettling circumstances."
+            "In the remote, usually tranquil village of Silverpine, people have been " +
+            "mysteriously vanishing during the night. The players start as a group of " +
+            "investigators hired by the desperate villagers to discover the source of " +
+            "these unsettling circumstances."
         },
         new object[]
         {
             "A deadly, visibly moving fog known as the Ashen Veil is swallowing cities one by one. " +
-            "The players begin as adventurers brought together by fate in a small town on the edge of " +
-            "the fog's destructive path, tasked with finding a way to halt the approaching doom."
+            "The players begin as adventurers brought together by fate in a small town on the edge " +
+            "of the fog's destructive path, tasked with finding a way to halt the approaching doom."
         },
         new object[]
         {
-            "An ancient, long-forgotten deity has been awakened, threating to engulf the world into chaos " +
-            "and darkness. The players are chosen by an opposing deity and begin their journey at a " +
-            "secluded temple, receiving their divine mission from the temple's oracle."
+            "An ancient, long-forgotten deity has been awakened, threating to engulf the world " +
+            "into chaos and darkness. The players are chosen by an opposing deity and begin their " +
+            "journey at a secluded temple, receiving their divine mission from the temple's oracle."
         },
         new object[]
         {
-            "The legendary submerged city of Zur, believed to be a myth, has risen from the depths of the " +
-            "ocean, bringing about seismic disturbances around the world and causing sea creatures to " +
-            "behave abnormally. The players start their adventure as part of a commissioned exploration " +
-            "team, tasked by the Grand Council to investigate this unprecedented event."
+            "The legendary submerged city of Zur, believed to be a myth, has risen from the " +
+            "depths of the ocean, bringing about seismic disturbances around the world and " +
+            "causing sea creatures to behave abnormally. The players start their adventure " +
+            "as part of a commissioned exploration team, tasked by the Grand Council to " +
+            "investigate this unprecedented event."
         },
     };
 }
