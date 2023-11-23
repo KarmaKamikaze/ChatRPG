66b6d7bab0f6fe3bd892916779a1ebc2d2ff8bc1
Sarmisuper <sarmilan3@gmail.com>
2023-11-23 14:06:33 +0100

    added comments and fixed queries

--- ChatRPG/Services/SystemPromptType.cs
+++ ChatRPG/Services/SystemPromptType.cs
@@ -2,5 +2,6 @@
 
 public enum SystemPromptType
 {
+    // For example, CombatHitMiss defines the systemprompt where the player hits with their attack and the opponent misses.
     Default, CombatHitHit, CombatHitMiss, CombatMissHit, CombatMissMiss, CombatMiddleComputation
 }
