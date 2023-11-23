6f6b295e2a16a6023e7f2953908acce322b21042
Nikolaj Dam <nikolajdam0@gmail.com>
2023-11-23 11:51:21 +0100

    if it does not work then it was sarmi

--- ChatRPGTests/CharacterTests.cs
+++ ChatRPGTests/CharacterTests.cs
@@ -1,6 +1,24 @@
+using ChatRPG.Data.Models;
+using Environment = ChatRPG.Data.Models.Environment;
+
 namespace ChatRPGTests;
 
 public class CharacterTests
 {
-    
+    [Theory]
+    [InlineData(-200, 0)]
+    [InlineData(-20, 80)]
+    [InlineData(-100, 0)]
+    [InlineData(10, 100)]
+    [InlineData(-5, 95)]
+    public void AdjustHealth_VariousInput_StaysWithinValidRange0To100(int adjustByAmount, int expectedCurrentHealth)
+    {
+        Campaign campaign = new Campaign(new User(), "");
+        Character character = new Character(campaign, new Environment(campaign, "", ""), CharacterType.Humanoid, "", "",
+            true, 100);
+
+        character.AdjustHealth(adjustByAmount);
+
+        Assert.Equal(expectedCurrentHealth, character.CurrentHealth);
+    }
 }
