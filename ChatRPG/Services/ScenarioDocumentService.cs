e47a3af1d6d887745cedca3f9478d6c8eba29c98
Nicolai Jørgensen <nicolaijgn@gmail.com>
2025-03-10 10:37:40 +0100

    Fix comment

--- ChatRPG/Services/ScenarioDocumentService.cs
+++ ChatRPG/Services/ScenarioDocumentService.cs
@@ -41,7 +41,7 @@ public class ScenarioDocumentService
             dimensions: 1536,
             dataSource: DataSource.FromBytes(scenarioDocument),
             collectionName: "~collection-" + campaignId,
-            // Second, configure how to extract chunks from the bigger document.
+            // Configure how to extract chunks from the bigger document.
             textSplitter: new RecursiveCharacterTextSplitter(
                 chunkSize: 500, // To pick the chunk size, estimate how much information would be required to capture most passages you'd like to ask questions about.  Too many characters makes it difficult to capture semantic meaning, and too few characters means you are more likely to split up important points that are related. In general, 200-500 characters is good for stories without complex sequences of actions.
                 chunkOverlap: 200)); // To pick the chunk overlap you need to estimate the size of the smallest piece of information. It may happen that one chunk ends with `Ron's hair` and the other one starts with `is red`.In this case, an embedding would miss important context, and not be generated properly. With overlap the end of the first chunk will appear in the beginning of the other, eliminating the problem.
